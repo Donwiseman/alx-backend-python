40732c4d6569adf013b3a227e62b06c009c46e1c
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-12-09 15:38:28 +0100

    Annotated the add function in 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This contains a function that returns the addittion of the given the two 
+    parameters received. """
+
+
+def add(a: float, b: float) -> float :
+    return a + b
