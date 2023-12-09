bc67cc5b731b0fc29dba99e622ff350fb28b63b2
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-12-09 17:15:36 +0100

    Created floor function in 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Contains a function that returns the floor of the given float parameter"""
+
+import math
+
+def floor(n: float) -> int:
+    """Returns the the floor of the passed float parameter"""
+    return math.floor(n)
