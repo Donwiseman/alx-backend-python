df96742c6aa1464eeca56fa354187fa0ab44c82e
Emmanuel Adaja <emmanueladaja@yahoo.com>
2023-12-09 16:58:33 +0100

    Created the concat function in 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Contains a function that conactenates two string parameters"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns the concatenation of borth strings"""
+    return f"{str1}{str2}"
