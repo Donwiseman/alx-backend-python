8cbc820876ea7522013d3addb6b22d4af7f97522
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2023-12-09 07:01:52 -0800

    Corected pycode errors in 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-"""This contains a function that returns the addittion of the given the two 
+"""This contains a function that returns the addittion of the given the two
     parameters received. """
 
 
-def add(a: float, b: float) -> float :
+def add(a: float, b: float) -> float:
+    """ returns sum of parameters."""
     return a + b
