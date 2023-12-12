e217b24f0b89f1194493aa04ed0f460e4f279783
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2023-12-12 07:57:30 -0800

    0-async_generator.py correction for mypy error

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[int, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """ A Async Generator for 10 random numbers. """
     for run in range(10):
         await asyncio.sleep(1)
