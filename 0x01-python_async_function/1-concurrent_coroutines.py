a5c812f3a93a6c1448568b0312e1e89132e41404
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2023-12-15 05:28:50 -0800

    1-concurrent_coroutines.py using asyncio.as_completed

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 """Introduces the Basic syntax of asyncio to perform concurrency"""
 
-wait_random = __import__('0-basic_async_syntax').wait_random
-import asyncio
 from typing import List
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Runs the wait_random coroutine ntimes"""
-    wait_times = await asyncio.gather(*(wait_random(max_delay) for i in range(n)))
+    """Runs the wait_random coroutine n times"""
+    tasks = [wait_random(max_delay) for i in range(n)]
+    wait_times = []
+    for future in asyncio.as_completed(tasks):
+        result = await future
+        wait_times.append(result)
     return wait_times
